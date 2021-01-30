9c25909fd0ee42e4d4d12b8cf9bb1cff7a0cdf33
jai-jk <jay.jk@hotmail.com>
2021-01-30 23:39:54 +0000

    Fix indentation

--- treasure-island.py
+++ treasure-island.py
@@ -40,19 +40,19 @@ print("Yar' mission is ta find tha' deep treasure..\n")
 choice1 = input("No time ta' waste! You see that divergin' path? Would ya like to go down the left one, or the right one?\n").lower()
 
 if choice1 == "left":
-  choice2 = (input("\nHmmm, looks like you've arrived at a simmering lava lake. Would ya like to use this new swimsuit I found, or wait? Type 'swim', or 'wait'.\n")).lower()
-  if choice2 == "wait":
-    choice3 = input("\nWell blow me down, yar' not half bad! It's not over yet though, avast ye! Ya see those dastardly, giant doors? Ya gotta choose one to find the deep treasure. Quick, choose! Red, yellow, blue.. or the secret one?\n").lower()
-    if choice3 == "yellow":
-      print("\nGood thinkin matey, ya' did it! Time for some hornswagglin!")
-    elif choice3 == "red":
-      print("\nDarn! Ya got burned by fiyaa! Game over for you, heartie!")
-    elif choice3 == "blue":
-      print("\nArghhh. You were eaten by a dastardly beast! Game over, and thanks for feedin' my pet.")
+    choice2 = (input("\nHmmm, looks like you've arrived at a simmering lava lake. Would ya like to use this new swimsuit I found, or wait? Type 'swim', or 'wait'.\n")).lower()
+    if choice2 == "wait":
+        choice3 = input("\nWell blow me down, yar' not half bad! It's not over yet though, avast ye! Ya see those dastardly, giant doors? Ya gotta choose one to find the deep treasure. Quick, choose! Red, yellow, blue.. or the secret one?\n").lower()
+        if choice3 == "yellow":
+            print("\nGood thinkin matey, ya' did it! Time for some hornswagglin!")
+        elif choice3 == "red":
+            print("\nDarn! Ya got burned by fiyaa! Game over for you, heartie!")
+        elif choice3 == "blue":
+            print("\nArghhh. You were eaten by a dastardly beast! Game over, and thanks for feedin' my pet.")
+        else:
+            print("\nWhy would ya ever think a secret door would be the answer! It's over for ya'!")
     else:
-      print("\nWhy would ya ever think a secret door would be the answer! It's over for ya'!")
-  else:
-    print("\nDarn! Why would you think the swimsuit would protect you against the lava trout! Don't say I didn't warn ya.")
+        print("\nDarn! Why would you think the swimsuit would protect you against the lava trout! Don't say I didn't warn ya.")
 else:
   print("\nWell done! Ya fell into quicksand. Game oveeeerrrrr.")
 
