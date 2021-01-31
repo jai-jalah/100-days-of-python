38807579fb400758723c4acd68efb21fa1c5047e
jai-jk <jay.jk@hotmail.com>
2021-01-31 00:36:42 +0000

    Rewrite/fix pseudo-code, write working loop for letter generator

--- password-generator.py
+++ password-generator.py
@@ -8,15 +8,23 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-# 1. new variables for letters, symbols, and numbers.
-# 2. for i in nr_letters + 1, selects random letter and adds it to variable.
+# 1. new list for letters, symbols, and numbers.
+# 2. for letters in range(1, nr_letters + 1), select random letter and insert in new list.
 # 3. same for nr_symbols and nr_numbers.
 # 4. combine all the variables together into a randomised list and join the list.
 # 5. print the password
+import random
+
+letter_ans = []
+symbol_ans = []
+number_ans = []
+
+for letter in range(1, nr_letters + 1):
+    letter_ans.append(letters[random.randint(0, nr_letters - 1)])
