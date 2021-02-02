2e1a6717a2317af0a4b15e9ac61b311a9d4fab8d
jai-jk <jay.jk@hotmail.com>
2021-02-02 22:28:39 +0000

    Add steps 1 - 6 from the flowchart

--- day-7-hangman-game.py
+++ day-7-hangman-game.py
@@ -1,16 +1,37 @@
-#Step 1
-word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 import random
 
-random_word = random.choice(word_list)
+hanger = '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+========='''
+body_part_list = ["|", "O", "|", "/", "\\", "/", "\\"]
+counter = -1
+word_list = ["aardvark", "baboon", "camel"]
 
-guess = input("Guess a letter.\n").lower()
+random_word = random.choice(word_list)
+answer = list(random_word)
+blank = "_"
+blank_answer = []
+body_parts_used = []
+
+for i in range(len(answer)):
+    blank_answer.append(blank)
+
+if counter != len(answer):
+    guess = input("Guess a letter.\n").lower()
+    counter += 1
 
 if guess in random_word:
-    print(True)
+    print()
+else:
+    print("That was incorrect.")
+    print(" ".join(blank_answer))
+    body_parts_used.append(body_part_list[counter + 1])
+    print(body_parts_used)
+
+
+print(hanger)
