67121fc94baf1f0a990cb9794bdfe7dc62e0fce6
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-04-28 17:41:47 +0100

    Make a soup from html and get lowest price as a float

--- 47-amazon-price-tracker/main.py
+++ 47-amazon-price-tracker/main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import smtplib
 
 TARGET = "https://www.amazon.co.uk/Basketball-Elastic-Cushioning-Lightweight-Trainers/dp/B07KW1GBLR/ref=sr_1_6?dchild=1&keywords=basketball+shoes&qid=1619626899&sr=8-6"
 HEADERS = {
@@ -11,3 +12,11 @@ HEADERS = {
 response = requests.get(url=TARGET, headers=HEADERS)
 response.raise_for_status()
 webpage_html = response.text
+
+soup = BeautifulSoup(webpage_html, "lxml")
+soup_price = soup.find("span", id="priceblock_ourprice").get_text()
+if "-" in soup_price:
+    lowest_price = soup_price.split("-")
+    soup_price = float(lowest_price[0].strip("£"))
+
+print(soup_price)
