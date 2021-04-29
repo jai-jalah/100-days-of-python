b5424ab14c50864c89620fd4d20fac4033327782
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-04-29 16:22:46 +0100

    Basic cookie clicker bot

--- 48-cookie-clicker/main.py
+++ 48-cookie-clicker/main.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+import time
 
 chrome_driver_path = "/Users/JaiJk/Projects/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
-
 driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie = driver.find_element_by_id("bigCookie")
+
+for _ in range(10000000000):
+    cookie.click()
+
+driver.quit()
