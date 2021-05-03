1011be03be2ef79b981baaf4038719ed1db1e62b
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-03 08:45:12 +0100

    Request posts from API and pass them to index.html

--- 57-blog-jinja/main.py
+++ 57-blog-jinja/main.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+from post import Post
 import requests
+
+posts = requests.get("https://api.npoint.io/4af156202f984d3464c3").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=post_objects)
 
 
 if __name__ == "__main__":
