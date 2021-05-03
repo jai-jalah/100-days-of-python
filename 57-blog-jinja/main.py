358af72e029232a3970045636a998f942a85131f
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-03 08:48:10 +0100

    Add route and url builder for individual posts

--- 57-blog-jinja/main.py
+++ 57-blog-jinja/main.py
@@ -16,5 +16,14 @@ def home():
     return render_template("index.html", all_posts=post_objects)
 
 
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
