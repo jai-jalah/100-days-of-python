0778752c5641f2b6f53d0e60a8b8652b8166c6d0
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-05 22:45:10 +0100

    Send email to site owner on submission of contact form

--- 59-to-60-bootstrapped-blog-with-flask/main.py
+++ 59-to-60-bootstrapped-blog-with-flask/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+from decouple import config
 
 posts = requests.get("https://api.npoint.io/0067e63917ca7a5034d9").json()
+EMAIL = config("EMAIL")
+EMAIL_PASS = config("EMAIL_PASS")
 
 app = Flask(__name__)
 
@@ -29,12 +33,18 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, EMAIL_PASS)
+        connection.sendmail(EMAIL, EMAIL, email_message)
 
 
 if __name__ == "__main__":
