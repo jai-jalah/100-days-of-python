a36867e217c9fe0fccc726723a6972dc37e1f17e
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-08 14:24:32 +0100

    Integrate custom css file using super block

--- 62-coffee-and-wifi/main.py
+++ 62-coffee-and-wifi/main.py
@@ -6,18 +6,19 @@ from wtforms.validators import DataRequired
 import csv
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 Bootstrap(app)
 
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    cafe = StringField("Cafe name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
 # make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
+# e.g. You could use emojis ☕️/💪/✘/🔌
 # make all fields required except submit
 # use a validator to check that the URL field has a URL entered.
 # ---------------------------------------------------------------------------
@@ -29,7 +30,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route("/add")
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
@@ -37,18 +38,18 @@ def add_cafe():
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
+    return render_template("add.html", form=form)
 
 
-@app.route('/cafes')
+@app.route("/cafes")
 def cafes():
-    with open('cafe-data.csv', newline='') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
+    with open("cafe-data.csv", newline="") as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=",")
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    return render_template("cafes.html", cafes=list_of_rows)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
