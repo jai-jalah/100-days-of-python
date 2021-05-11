5e9264a0301a66151acca59119a97233178e7cc8
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-11 18:43:28 +0100

    Create db and alter /add route to add to the db instead of saving locally

--- 63-library/main.py
+++ 63-library/main.py
@@ -1,25 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", books=all_books)
+    return render_template("index.html")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["book-title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"],
-        }
-        all_books.append(new_book)
-        # print(all_books)
+        new_book = Book(
+            title=request.form["book-title"],
+            author=request.form["author"],
+            rating=request.form["rating"],
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for("home"))
 
     return render_template("add.html")
