711c3f53e3b4e7a98723f21439cd0e12163fbacd
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-12 16:03:34 +0100

    View movies from db on home page

--- 64-top-10-movies/main.py
+++ 64-top-10-movies/main.py
@@ -44,7 +44,8 @@ class Movie(db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
 
 
 if __name__ == "__main__":
