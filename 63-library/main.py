c354143b356bd766c8a76c926064fd67de7749b9
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-12 15:39:12 +0100

    Add option to delete book

--- 63-library/main.py
+++ 63-library/main.py
@@ -53,5 +53,14 @@ def edit():
     return render_template("edit.html", book=book_selected)
 
 
+@app.route("/delete")
+def delete_book():
+    book_id = request.args.get("id")
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
